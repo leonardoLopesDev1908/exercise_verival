9b26290356993ec34eec76bfb326284434fc6278
leonardoLopesDev1908 <leonardosilvalls1908@gmail.com>
2026-08-18 22:01:56 -0300

    changes

---
+++ src/main/java/com/example/Laugh.java
@@ -0,0 +1,35 @@
+package com.example;
+
+public class Laugh {
+    
+    public char exercise(String entrada) {
+        if (entrada == null) return 'N';
+            StringBuilder vogais = new StringBuilder();
+            for (char c : entrada.trim().toCharArray()) {
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                    vogais.append(c);
+                }
+            }
+
+            if (ehPalindromo(vogais.toString())) {
+                return 'S';
+            } else {
+                return 'N';
+            }
+        }
+
+    private static boolean ehPalindromo(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
